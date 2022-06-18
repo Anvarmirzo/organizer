37970749c957b5cff33839ecb150e31372284700
Anvarmirzo <64534079+Anvarmirzo@users.noreply.github.com>
2022-06-19 01:49:34 +0500

    updated dirname (#3)

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/organizer'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
     '/dist/<app-name>/index.html'));
